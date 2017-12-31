ae0f2f95dda1377116f255ccc1db1a36b80f28b7
Torkel <torkel@voysys.se>
2017-12-31 23:12:14 +0100

    100B.. nope...

--- pe_51/src/main.rs
+++ pe_51/src/main.rs
@@ -1,69 +1,95 @@
-use std::collections::HashMap;
+extern crate bit_vec;
+use bit_vec::BitVec;
+
+fn split_digits(n: i64) -> Vec<u8> {
+    fn x_inner(n: i64, xs: &mut Vec<u8>) {
+        if n >= 10 {
+            x_inner(n / 10, xs);
+        }
+        xs.push((n % 10) as u8);
+    }
+    let mut xs = Vec::new();
+    x_inner(n, &mut xs);
+    xs
+}
+
+fn join_digits(d: Vec<u8>) -> i64 {
+    let mut res: i64 = 0;
+    for dig in d {
+        res *= 10;
+        res += dig as i64;
+    }
+    return res
+}
 
 fn main() {
 
     let min: i64 =  9999999999;
-    let max: i64 = 49999999999;
-    let mut primes: Vec<i64> = Vec::new();
+    let max: i64 = 99999999999;
 
-    {
-        let mut prime_v: Vec<bool> = Vec::new();
-        for _i in 0..max {
-            prime_v.push(true);
-        }
+    let primes = {
 
-        println!("vector inited");
+        let mut bv = BitVec::from_elem(max as usize, true);
 
+        bv.set(0, false);
+        bv.set(1, false);
 
-        prime_v[1] = false;
-
-        for p in 2..max {
-            if prime_v[p as usize] {
-                if p > min {
-                    primes.push(p);
-                }
-                let mut c: i64 = 2 * p;
-                while c < max {
-                    prime_v[c as usize] = false;
-                    c += p;
+        for i in 2..(1 + (max as f64).sqrt() as usize) {
+            if bv[i] {
+                for j in i.. {
+                    if i * j >= (max as usize) {
+                        break;
+                    }
+                    bv.set(i * j, false)
                 }
             }
         }
+        bv
+    };
 
-        println!("primes calculated");
-    }
+    println!("primes calculated");
 
-    let mut saved_mutations: HashMap<String, i32> = HashMap::new();
+    // for p in 0..max {
+    //     if primes[p as usize] {
+    //         println!("{:?} {}", p, join_digits(split_digits(p)));
+    //     }
+    // }
 
-    let mut second_digit: char = '0';
+    for p in 0..max {
 
-    for p in primes {
-        let chars: Vec<char> = p.to_string().chars().collect();
+        if primes[p as usize] && p > min {
+            let digits = split_digits(p);
 
-        if chars.len() > 2 {
+            if digits.len() > 2 {
 
-            if chars[1] != second_digit {
-                second_digit = chars[1];
-                saved_mutations.clear();
-            }
-
-            for i1 in 1..(chars.len() - 2) {
-                for i2 in (i1 + 1)..(chars.len() - 1) {
-                    if chars[i1] == chars[i2] {
-                        let mut test_c = chars.clone();
-                        test_c[i1] = '*';
-                        test_c[i2] = '*';
-                        let test_s: String = test_c.iter().collect();
-                        if !saved_mutations.contains_key(&test_s) {
-                            saved_mutations.insert(test_s, 1);
-                        } else {
-                            let val: i32 = saved_mutations.get(&test_s).unwrap() + 1;
-                            saved_mutations.insert(test_s.clone(), val);
-                            if saved_mutations[&test_s] >= 7 {
-                                println!("{}: {}", &saved_mutations[&test_s], test_s);
+                for i1 in 0..(digits.len() - 2) {
+                    for i2 in (i1 + 1)..(digits.len() - 1) {
+                        if digits[i1] == digits[i2] {
+                            let mut count = 0;
+                            let mut start_i = 0;
+                            if i1 == 0 {
+                                start_i = 1;
                             }
-                            if saved_mutations[&test_s] == 8 {
-                                return;
+                            for i in start_i..10 {
+                                let mut test_d = digits.clone();
+                                test_d[i1] = i;
+                                test_d[i2] = i;
+                                let test_dig = join_digits(test_d);
+                                if test_dig < max && primes[test_dig as usize] {
+                                    count += 1;
+                                }
+                            }
+                            if count >= 7 {
+                                let mut test_d1 = digits.clone();
+                                test_d1[i1] = 0;
+                                test_d1[i2] = 0;
+                                let mut test_d2 = digits.clone();
+                                test_d2[i1] = 1;
+                                test_d2[i2] = 1;
+                                println!("{}: {} / {} / {}", count, join_digits(digits.clone()), join_digits(test_d1), join_digits(test_d2));
+                            }
+                            if count >= 8 {
+                                return
                             }
                         }
                     }
