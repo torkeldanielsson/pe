a3834fb4cf613d5cb3e0c3defe7cc2ea5c2f9f2a
Torkel <torkel@voysys.se>
2017-12-31 17:42:26 +0100

    kill 9

--- pe_51/src/main.rs
+++ pe_51/src/main.rs
@@ -1,83 +1,70 @@
-use std::char;
-
-fn is_prime(n: i64) -> bool {
-
-    if n == 2 {
-        return true;
-    }
-
-    if n % 2 == 0 || n <= 1 {
-        return false;
-    }
-
-    let mut i: i64 = 3;
-
-    while i <= (n as f64).sqrt() as i64 {
-
-        if n % i == 0 {
-            return false;
-        }
-
-        i += 2;
-    }
-
-    return true;
-}
+use std::collections::HashMap;
 
 fn main() {
 
-    let min: i64 =  10000000;
-    let max: i64 = 999999999;
+    let min: i64 =  9999999999;
+    let max: i64 = 49999999999;
+    let mut primes: Vec<i64> = Vec::new();
 
-    let mut primes = Vec::new();
-    let mut c: i64 = min - 1;
-    while c < (max + 1) {
-        c += 2;
-        if is_prime(c) {
-            primes.push(c);
+    {
+        let mut prime_v: Vec<bool> = Vec::new();
+        for _i in 0..max {
+            prime_v.push(true);
         }
+
+        println!("vector inited");
+
+
+        prime_v[1] = false;
+
+        for p in 2..max {
+            if prime_v[p as usize] {
+                if p > min {
+                    primes.push(p);
+                }
+                let mut c: i64 = 2 * p;
+                while c < max {
+                    prime_v[c as usize] = false;
+                    c += p;
+                }
+            }
+        }
+
+        println!("primes calculated");
     }
-    primes.sort();
 
-    println!("generated primes");
+    let mut saved_mutations: HashMap<String, i32> = HashMap::new();
 
-    let mut tested = Vec::new();
+    let mut second_digit: char = '0';
 
-    for p in primes.clone() {
-        let mut chars: Vec<char> = p.to_string().chars().collect();
-        for i1 in 0..(chars.len() - 2) {
-            for i2 in (i1 + 1)..(chars.len() - 1) {
-                if chars[i1] == chars[i2] {
-                    let mut test_c = chars.clone();
-                    test_c[i1] = '*';
-                    test_c[i2] = '*';
-                    if !tested.contains(&test_c) {
-                        tested.push(test_c.clone());
+    for p in primes {
+        let chars: Vec<char> = p.to_string().chars().collect();
 
-                        let mut count = 0;
-                        let mut digs = Vec::new();
-                        let mut start_n = 0;
-                        if i1 == 0 {
-                            start_n = 1;
-                        }
-                        for n in start_n..10 {
-                            chars[i1] = char::from_digit(n, 10).unwrap();
-                            chars[i2] = char::from_digit(n, 10).unwrap();
-                            let s: String = chars.iter().collect();
-                            let to_test: i64 = s.parse::<i64>().unwrap();
-                            if primes.contains(&to_test) {
-                                count += 1;
-                                digs.push(n);
+        if chars.len() > 2 {
+
+            if chars[1] != second_digit {
+                second_digit = chars[1];
+                saved_mutations.clear();
+            }
+
+            for i1 in 1..(chars.len() - 2) {
+                for i2 in (i1 + 1)..(chars.len() - 1) {
+                    if chars[i1] == chars[i2] {
+                        let mut test_c = chars.clone();
+                        test_c[i1] = '*';
+                        test_c[i2] = '*';
+                        let test_s: String = test_c.iter().collect();
+                        if !saved_mutations.contains_key(&test_s) {
+                            saved_mutations.insert(test_s, 1);
+                        } else {
+                            let val: i32 = saved_mutations.get(&test_s).unwrap() + 1;
+                            saved_mutations.insert(test_s.clone(), val);
+                            if saved_mutations[&test_s] >= 7 {
+                                println!("{}: {}", &saved_mutations[&test_s], test_s);
                             }
-                        }
-
-                        if count >= 8 {
-                            let s: String = test_c.iter().collect();
-                            print!("{}: {} -", count, s);
-                            for d in digs {
-                                print!(" {}", d);
+                            if saved_mutations[&test_s] == 8 {
+                                return;
                             }
-                            println!("");
                         }
                     }
                 }
