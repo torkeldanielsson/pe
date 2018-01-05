2e2db2fced0f58374c9b36c6711dbd74581082a0
Torkel <torkel@voysys.se>
2018-01-05 11:40:00 +0100

    wip

--- pe_51/src/main.rs
+++ pe_51/src/main.rs
@@ -1,5 +1,6 @@
 extern crate bit_vec;
 use bit_vec::BitVec;
+use std::collections::HashSet;
 
 fn split_digits(n: i64) -> Vec<u8> {
     fn x_inner(n: i64, xs: &mut Vec<u8>) {
@@ -24,69 +25,135 @@ fn join_digits(d: Vec<u8>) -> i64 {
 
 fn main() {
 
-    let min: i64 =  9999999999;
-    let max: i64 = 99999999999;
+    for max_o in 1..5 {
+        let mut max: usize = 10;
+        for _i in 0..max_o {
+            max *= 10;
+        }
+        let min: usize = max / 10;
 
-    let primes = {
+        println!("checking {} - {}", min, max);
 
-        let mut bv = BitVec::from_elem(max as usize, true);
+        let limit = (max as f64).sqrt() as usize + 1;
+        let sievebound: usize = (limit - 1) / 2;  // last index of sieve
+        let mut sieve = BitVec::from_elem(sievebound as usize, true);
+        let crosslimit: usize = ((limit as f64).sqrt() as usize + 1) / 2;
 
-        bv.set(0, false);
-        bv.set(1, false);
+        for d in 1..crosslimit {
+            if sieve[d] { // 2*i + 1 is prime, mark multiples
 
-        for i in 2..(1 + (max as f64).sqrt() as usize) {
-            if bv[i] {
-                for j in i.. {
-                    if i * j >= (max as usize) {
-                        break;
-                    }
-                    bv.set(i * j, false)
+                let mut i: usize = 2 * d * (d + 1);
+                while i < sievebound {
+                    sieve.set(i, false);
+                    i += 2*d + 1;
                 }
             }
         }
-        bv
-    };
 
-    println!("primes calculated");
+        let mut div_primes = Vec::new();
+        div_primes.push(2);
 
-    // for p in 0..max {
-    //     if primes[p as usize] {
-    //         println!("{:?} {}", p, join_digits(split_digits(p)));
-    //     }
-    // }
+        for pi in 1..(sievebound - 1) {
+            if sieve[pi] {
+                let p = 2*pi + 1;
 
-    for p in 0..max {
+                div_primes.push(p);
+            }
+        }
 
-        if primes[p as usize] && p > min {
-            let digits = split_digits(p);
+        println!("div_primes ready");
 
-            if digits.len() > 2 {
+        let mut p = min - 2;
+        if p % 2 == 0 {
+            p += 1;
+        }
 
-                for i1 in 0..(digits.len() - 2) {
-                    for i2 in (i1 + 1)..(digits.len() - 1) {
+        let precomp_primes_range: usize = 100000000;
+        let mut precomp_primes_start: usize = 0;
+        let mut precomp_primes_end: usize = 0;
+        let mut precomp_primes = HashSet::new();
+
+        let mut printed_results = HashSet::new();
+
+        'outer: while p < max {
+            p += 2;
+
+            if p > precomp_primes_end || p < precomp_primes_start {
+                precomp_primes_start = p;
+                precomp_primes_end = p + precomp_primes_range;
+
+                if precomp_primes_end > max {
+                    precomp_primes_end = max;
+                }
+
+                precomp_primes = HashSet::new();
+                
+                'precomp: for pi in precomp_primes_start..(precomp_primes_end + 1) {
+                    'pcf: for pri in &div_primes {
+                        if pri >= &pi {
+                            break 'pcf;
+                        }
+                        if pi % pri == 0 {
+                            continue 'precomp;
+                        }
+                    }
+                    precomp_primes.insert(pi);
+                }
+                println!("precomped {} - {}", precomp_primes_start, precomp_primes_end);
+            }
+
+            if !precomp_primes.contains(&p) {
+                continue 'outer;
+            }
+
+            let digits = split_digits(p as i64);
+
+            if digits.len() > 1 {
+
+                for i1 in 0..(digits.len() - 1) {
+                    for i2 in (i1 + 1)..(digits.len() - 0) {
                         if digits[i1] == digits[i2] {
                             let mut count = 0;
                             let mut start_i = 0;
                             if i1 == 0 {
                                 start_i = 1;
                             }
-                            for i in start_i..10 {
+                            'inner: for i in start_i..10 {
                                 let mut test_d = digits.clone();
                                 test_d[i1] = i;
                                 test_d[i2] = i;
-                                let test_dig = join_digits(test_d);
-                                if test_dig < max && primes[test_dig as usize] {
+                                let test_dig = join_digits(test_d) as usize;
+                                if test_dig < precomp_primes_start || test_dig > precomp_primes_end {
+                                    for pri in &div_primes {
+                                        if pri < &test_dig && test_dig % pri == 0 {
+                                            continue 'inner;
+                                        }
+                                    }
                                     count += 1;
+                                } else {
+                                    if precomp_primes.contains(&test_dig) {
+                                        count += 1;
+                                    }
                                 }
                             }
                             if count >= 7 {
-                                let mut test_d1 = digits.clone();
-                                test_d1[i1] = 0;
-                                test_d1[i2] = 0;
-                                let mut test_d2 = digits.clone();
-                                test_d2[i1] = 1;
-                                test_d2[i2] = 1;
-                                println!("{}: {} / {} / {}", count, join_digits(digits.clone()), join_digits(test_d1), join_digits(test_d2));
+                                if !printed_results.contains(&p) {
+                                    print!("{:?}:", count);
+                                    'inner2: for i in start_i..10 {
+                                        let mut test_d = digits.clone();
+                                        test_d[i1] = i;
+                                        test_d[i2] = i;
+                                        let test_dig = join_digits(test_d) as usize;
+                                        for pri in &div_primes {
+                                            if pri < &test_dig && test_dig % pri == 0 {
+                                                continue 'inner2;
+                                            }
+                                        }
+                                        printed_results.insert(test_dig);
+                                        print!(" {:?}", test_dig);
+                                    }
+                                    println!("");
+                                }
                             }
                             if count >= 8 {
                                 return
